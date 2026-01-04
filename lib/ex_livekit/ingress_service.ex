57f1fb9d8177e1d964cca2bf31571cc981298521
Adrian Borowski <adrian.borowski@appunite.com>
2026-01-04 20:26:37 +0100

    removed proto resposnse struct for ingress listing functions and improved docs

--- lib/ex_livekit/ingress_service.ex
+++ lib/ex_livekit/ingress_service.ex
@@ -14,7 +14,6 @@ defmodule ExLivekit.IngressService do
     DeleteIngressRequest,
     IngressInfo,
     ListIngressRequest,
-    ListIngressResponse,
     UpdateIngressRequest
   }
 
@@ -24,28 +23,52 @@ defmodule ExLivekit.IngressService do
   @type ingress_id :: String.t()
 
   @type create_ingress_opts :: [
-          input_type: Livekit.IngressInput.t(),
+          input_type: :RTMP_INPUT | :WHIP_INPUT | :URL_INPUT,
           url: String.t(),
           name: String.t(),
           room_name: String.t(),
           participant_identity: String.t(),
           participant_name: String.t(),
-          participant_metadata: String.t()
+          enable_transcoding: boolean(),
+          audio: Livekit.IngressAudioOptions.t(),
+          video: Livekit.IngressVideoOptions.t()
         ]
   @doc """
   Creates a new ingress.
 
-  Options:
-  - input_type: the type of input to use for the ingress
-  - url: the url to use for the ingress
-  - name: the name to use for the ingress
+  Input Options:
+  - input_type: :RTMP_INPUT or :WHIP_INPUT (required)
+  - url: HTTP(S) or SRT url to the file or stream (only for URL_INPUT)
+  - name: optional, name to identify the ingress
+  - room_name: the room name to publish to
+  - participant_identity: Unique identity for the room participant the Ingress service will connect as
+  - participant_name: Name displayed in the room for the participant
+  - enable_transcoding: optional, whether to enable transcoding or forward the input media directly. Transcoding is required for all input types except WHIP. For WHIP, the default is to not transcode.
+  - audio: optional, audio options for the ingress
+  - video: optional, video options for the ingress
+
+
   @spec create_ingress(Client.t(), create_ingress_opts()) :: {:ok, IngressInfo.t()} | {:error, Error.t()}
-  ## Examples
+  ## RTMP Example
 
   ```elixir
-  {:ok, ingress} = ExLivekit.IngressService.create_ingress(client, input_type: :URL_INPUT, url: "https://example.com/stream.mp4", name: "example", room_name: "room_name", participant_identity: "participant_identity", participant_name: "participant_name", participant_metadata: "participant_metadata")
+  {:ok, ingress} = ExLivekit.IngressService.create_ingress(client, input_type: :RTMP_INPUT, name: "example", room_name: "room_name", participant_identity: "participant_identity", participant_name: "participant_name", enable_transcoding: true)
+  ```
+
+  ## WHIP Example
+
+  ```elixir
+  {:ok, ingress} = ExLivekit.IngressService.create_ingress(client, input_type: :WHIP_INPUT, name: "example", room_name: "room_name", participant_identity: "participant_identity", participant_name: "participant_name", enable_transcoding: false)
+  ```
+
+  ## URL Example
+
+  ```elixir
+  {:ok, ingress} = ExLivekit.IngressService.create_ingress(client, input_type: :URL_INPUT, url: "https://example.com/stream.mp4", name: "example", room_name: "room_name", participant_identity: "participant_identity", participant_name: "participant_name", enable_transcoding: true)
   ```
   """
+  @spec create_ingress(Client.t(), create_ingress_opts()) ::
+          {:ok, IngressInfo.t()} | {:error, Error.t()}
   def create_ingress(%Client{} = client, opts \\ []) do
     auth_headers = Client.auth_headers(client, video_grant: %VideoGrant{ingress_admin: true})
 
@@ -56,7 +79,9 @@ defmodule ExLivekit.IngressService do
       room_name: opts[:room_name],
       participant_identity: opts[:participant_identity],
       participant_name: opts[:participant_name],
-      participant_metadata: opts[:participant_metadata]
+      enable_transcoding: opts[:enable_transcoding],
+      audio: opts[:audio],
+      video: opts[:video]
     }
 
     case Client.request(client, @svc, "CreateIngress", payload, auth_headers) do
@@ -66,49 +91,53 @@ defmodule ExLivekit.IngressService do
   end
 
   @type update_ingress_opts :: [
-          ingress_id: ingress_id(),
           name: String.t(),
           room_name: String.t(),
           participant_identity: String.t(),
           participant_name: String.t(),
-          participant_metadata: String.t()
+          enable_transcoding: boolean(),
+          audio: Livekit.IngressAudioOptions.t(),
+          video: Livekit.IngressVideoOptions.t()
         ]
   @doc """
   Updates an existing ingress.
 
   Options:
-  - ingress_id: the id of the ingress to update
-  - name: the name to update
-  - room_name: the room name to update
-  - participant_identity: the participant identity to update
-  - participant_name: the participant name to update
-  - participant_metadata: the participant metadata to update
+  - name: optional, name to identify the ingress
+  - room_name: optional, the room name to publish to
+  - participant_identity: optional, Unique identity for the room participant the Ingress service will connect as
+  - participant_name: optional, Name displayed in the room for the participant
+  - enable_transcoding: optional, whether to enable transcoding or forward the input media directly. Transcoding is required for all input types except WHIP. For WHIP, the default is to not transcode.
+  - audio: optional, audio options for the ingress
+  - video: optional, video options for the ingress
 
   ## Examples
 
   ```elixir
   {:ok, ingress} = ExLivekit.IngressService.update_ingress(client,
-    ingress_id: "ingress_id",
+    "ingress_id",
     name: "example",
     room_name: "room_name",
     participant_identity: "participant_identity",
     participant_name: "participant_name",
-    participant_metadata: "participant_metadata"
+    enable_transcoding: true
   )
   ```
   """
-  @spec update_ingress(Client.t(), update_ingress_opts()) ::
+  @spec update_ingress(Client.t(), ingress_id(), update_ingress_opts()) ::
           {:ok, IngressInfo.t()} | {:error, Error.t()}
-  def update_ingress(%Client{} = client, opts \\ []) do
+  def update_ingress(%Client{} = client, ingress_id, opts \\ []) do
     auth_headers = Client.auth_headers(client, video_grant: %VideoGrant{ingress_admin: true})
 
     payload = %UpdateIngressRequest{
-      ingress_id: opts[:ingress_id],
+      ingress_id: ingress_id,
       name: opts[:name],
       room_name: opts[:room_name],
       participant_identity: opts[:participant_identity],
       participant_name: opts[:participant_name],
-      participant_metadata: opts[:participant_metadata]
+      enable_transcoding: opts[:enable_transcoding],
+      audio: opts[:audio],
+      video: opts[:video]
     }
 
     case Client.request(client, @svc, "UpdateIngress", payload, auth_headers) do
@@ -135,15 +164,19 @@ defmodule ExLivekit.IngressService do
   ```
   """
   @spec list_ingresses(Client.t(), list_ingresses_opts()) ::
-          {:ok, ListIngressResponse.t()} | {:error, Error.t()}
+          {:ok, [IngressInfo.t()]} | {:error, Error.t()}
   def list_ingresses(%Client{} = client, opts \\ []) do
     auth_headers = Client.auth_headers(client, video_grant: %VideoGrant{ingress_admin: true})
 
     payload = %ListIngressRequest{room_name: opts[:room_name], ingress_id: opts[:ingress_id]}
 
     case Client.request(client, @svc, "ListIngress", payload, auth_headers) do
-      {:ok, body} -> {:ok, Livekit.ListIngressResponse.decode(body)}
-      {:error, error} -> {:error, error}
+      {:ok, body} ->
+        resp = Livekit.ListIngressResponse.decode(body)
+        {:ok, resp.items}
+
+      {:error, error} ->
+        {:error, error}
     end
   end
 
